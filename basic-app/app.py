1fd1ce2fe44d76cfcfce645acd7158933658ecc3
akafuh <itsmefuh@gmail.com>
2026-08-03 22:43:30 +0500

    basic-app

--- basic-app/app.py
+++ basic-app/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "hello, world"
+def index():
+    return render_template("index.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
